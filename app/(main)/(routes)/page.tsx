de81caaaf18425d24fbc9c137b3008c34308a26e
ECarry <lianshiliang93@gmail.com>
2023-09-10 01:08:11 +0800

    feat: dark mode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,12 @@
+import { ModeToggle } from "@/components/ModeToggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex justify-between items-center">
       This is a protected route.
-      <div>
+      <div className="flex items-center gap-4">
+        <ModeToggle />
         <UserButton afterSignOutUrl="/"/>
       </div>
     </div>
