093355141339f5dcb1bcfa36ccf601e9d0c8262b
ECarry <lianshiliang93@gmail.com>
2023-09-20 22:26:27 +0800

    fix: Socket disconnect

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -1,7 +1,8 @@
-import { NextApiResponseServerIo } from '@/types'
 import { Server as NetServer } from 'http'
 import { NextApiRequest } from 'next'
 import { Server as ServerIO } from 'socket.io'
+
+import { NextApiResponseServerIo } from '@/types'
 
 export const config = {
   api: {
@@ -11,7 +12,7 @@ export const config = {
 
 const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
   if (!res.socket.server.io) {
-    const path = 'api/socket/io'
+    const path = '/api/socket/io'
     const httpServer: NetServer = res.socket.server as any
     const io = new ServerIO(httpServer, {
       path: path,
